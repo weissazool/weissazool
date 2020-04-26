99d5f33c4cabe68992d6fc0ff1814a66dc34a03e
Anthony Weiss <aweiss42@gmail.com>
2020-04-26 11:08:32 +0900

    Shorten indenting

--- Sources/Weissazool/main.swift
+++ Sources/Weissazool/main.swift
@@ -4,33 +4,33 @@ import Plot
 
 // This type acts as the configuration for your website.
 struct Weissazool: Website {
-    enum SectionID: String, WebsiteSectionID {
-        // Add the sections that you want your website to contain here:
-        case blog
-        case projects
-    }
-
-    struct ItemMetadata: WebsiteItemMetadata {
-        // Add any site-specific metadata that you want to use here.
-    }
-
-    // Update these properties to configure your website:
-    var url = URL(string: "https://weissazool.github.io/try-publish")!
-    var name = "Weissazool Dot Com"
-    var description = "Dusting off my little corner of the internet"
-    var language: Language { .english }
-    var imagePath: Path? { nil }
+  enum SectionID: String, WebsiteSectionID {
+    // Add the sections that you want your website to contain here:
+    case blog
+    case projects
+  }
+  
+  struct ItemMetadata: WebsiteItemMetadata {
+    // Add any site-specific metadata that you want to use here.
+  }
+  
+  // Update these properties to configure your website:
+  var url = URL(string: "https://weissazool.github.io/try-publish")!
+  var name = "Weissazool Dot Com"
+  var description = "Dusting off my little corner of the internet"
+  var language: Language { .english }
+  var imagePath: Path? { nil }
 }
 
 
 try Weissazool().publish(using: [
-    .addMarkdownFiles(),
-    // Posts are in LIFO order
-    .sortItems(by: \.date, order: .descending),
-    // Copy images to the output folder
-    .copyResources(),
-    // Generate the website with my custom theme
-    .generateHTML(withTheme: .weissazool),
-    // Deploy to Github Pages -- ideally, in the future. There are some PRs for this.
-    .deploy(using: .gitHub("weissazool/try-publish"))
+  .addMarkdownFiles(),
+  // Posts are in LIFO order
+  .sortItems(by: \.date, order: .descending),
+  // Copy images to the output folder
+  .copyResources(),
+  // Generate the website with my custom theme
+  .generateHTML(withTheme: .weissazool),
+  // Deploy to Github Pages -- ideally, in the future. There are some PRs for this.
+  .deploy(using: .gitHub("weissazool/try-publish"))
 ])
