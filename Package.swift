fb965a9d0006182c0a1aea16339056ab2c79bf0d
Anthony Weiss <aweiss42@gmail.com>
2026-02-09 22:02:12 +0900

    Update swift-tools and Publish
    Because the folders are broken when I deploy. I thought updating
    dependencies might be the path of least resistance. And if not, at
    least my dependencies will be updated.

--- Package.swift
+++ Package.swift
@@ -1,19 +1,24 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
-    name: "Weissazool",
-    products: [
-        .executable(name: "Weissazool", targets: ["Weissazool"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.7.0")
-    ],
-    targets: [
-        .target(
-            name: "Weissazool",
-            dependencies: ["Publish"]
-        )
-    ]
+  name: "Weissazool",
+  platforms: [
+    .macOS(.v12)
+  ],
+  products: [
+    .executable(name: "Weissazool", targets: ["Weissazool"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
+  ],
+  targets: [
+    .executableTarget(
+      name: "Weissazool",
+      dependencies: [
+        .product(name: "Publish", package: "publish")
+      ]
+    )
+  ]
 )
